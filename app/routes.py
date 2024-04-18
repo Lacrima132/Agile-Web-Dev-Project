a69476fd17eebe46c9013138faa2dd7cd2a39f62
ipr17 <23159504@student.uwa.edu.au>
2024-04-18 15:37:12 +0800

    fixed links between web pages, added routes to each web page

--- app/routes.py
+++ app/routes.py
@@ -2,8 +2,34 @@ from app import app
 from flask import render_template, url_for
 
 @app.route('/')
-@app.route('/home')
+@app.route('/index')
 @app.route('/user/<name>')
-def user():
+def index():
     name = 'User'
     return render_template('index.html', name=name)
+
+@app.route('/browse')
+def browse():
+    name = 'User'
+    return render_template('browse.html', name=name)
+
+@app.route('/faq')
+def faq():
+    name = 'User'
+    return render_template('faq.html', name=name)
+
+@app.route('/login')
+def login():
+    name = 'User'
+    return render_template('login.html', name=name)
+
+@app.route('/ranking')
+def ranking():
+    name = 'User'
+    return render_template('ranking.html', name=name)
+
+@app.route('/submit')
+def submit():
+    name = 'User'
+    return render_template('submit.html', name=name)
+
