51c4dfe227d74f8ee5748657ddeee78afcd6980c
ipr17 <23159504@student.uwa.edu.au>
2024-04-29 17:42:04 +0800

    added lightsaber cursor and simple functionality to profile page

--- app/static/js/indexpage.js
+++ app/static/js/indexpage.js
@@ -6,3 +6,29 @@ function shrinkNav() {
   document.querySelector(".sidebar").style.width =
     "4%";
 }
+function currentPosts() {
+  var curr = document.getElementById("current");
+  var work = document.getElementById("working");
+  var prev = document.getElementById("previous");
+  curr.style.display = "grid";
+  work.style.display = "none";
+  prev.style.display = "none";
+}
+
+function workingPosts() {
+  var curr = document.getElementById("current");
+  var work = document.getElementById("working");
+  var prev = document.getElementById("previous");
+  curr.style.display = "none";
+  work.style.display = "grid";
+  prev.style.display = "none";
+}
+
+function previousPosts() {
+  var curr = document.getElementById("current");
+  var work = document.getElementById("working");
+  var prev = document.getElementById("previous");
+  curr.style.display = "none";
+  work.style.display = "none";
+  prev.style.display = "grid";
+}
