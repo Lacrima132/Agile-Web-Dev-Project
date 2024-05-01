e7b3b4c24c20d97065b55486b6e284e875e41cf4
Daniel <loodaniel06@gmail.com>
2024-05-01 16:46:35 +0800

    Added proper formatting and style to browse page. Developed post boxes with like count, comment count and view count. Added css including likes and nav bar animations. Added javascript to implement 'likes' functionality.

--- app/static/js/indexpage.js
+++ app/static/js/indexpage.js
@@ -6,3 +6,27 @@ function shrinkNav() {
   document.querySelector(".sidebar").style.width =
     "4%";
 }
+
+function revealHidden() {
+  const hiddenText =
+    document.querySelectorAll(".hidden");
+  hiddenText.forEach((el) =>
+    el.classList.add("reveal")
+  );
+}
+
+function hideHidden() {
+  const hiddenText =
+    document.querySelectorAll(".hidden");
+  hiddenText.forEach((el) =>
+    el.classList.remove("reveal")
+  );
+}
+
+function incrementLike() {
+  let likeCount = 0;
+  const likeCountDisplay =
+    document.getElementById("likeCount");
+  likeCount++;
+  likeCountDisplay.textContent = likeCount;
+}
---
+++ installer.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Create virtual environment
+
+python3 -m venv tmp-env
+
+# Activate virtual environment
+source tmp-env/bin/activate
+
+# Install Flask and its dependencies
+pip install flask
+pip install flask-login
+pip install flask-sqlalchemy
+
+# Run Flask application
+python3 -m flask run --debug
+
+#run file using ./installer.sh
