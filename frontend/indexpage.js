50fee7d7385e87970aed64eb85ac662b50cc886e
Daniel Loo <loodaniel06@gmail.com>
2024-05-01 09:41:55 +0800

    Added to the browsing page and posts with grid. Implemented animations for nav bar.

---
+++ frontend/indexpage.js
@@ -0,0 +1,23 @@
+function growNav() {
+  document.querySelector(".sidebar").style.width = "15%";
+}
+function shrinkNav() {
+  document.querySelector(".sidebar").style.width = "4%";
+}
+
+function revealHidden() {
+  const hiddenText = document.querySelectorAll(".hidden");
+  hiddenText.forEach((el) => el.classList.add("reveal"));
+}
+
+function hideHidden() {
+  const hiddenText = document.querySelectorAll(".hidden");
+  hiddenText.forEach((el) => el.classList.remove("reveal"));
+}
+
+function incrementLike() {
+  let likeCount = 0;
+  const likeCountDisplay = document.getElementById("likeCount");
+  likeCount++;
+  likeCountDisplay.textContent = likeCount;
+}
